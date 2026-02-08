69fccb0c3d2fa94a0bc22753884d846c12b6b64d
ahmed04raxa <ahmed04raxa@gmail.com>
2026-02-08 19:41:06 +0500

    Lecture 01

---
+++ Lecture01/app.js
@@ -0,0 +1,30 @@
+// let n;
+
+// console.log(n, typeof (n));
+
+// let status = false;
+// console.log(status, typeof (status));
+
+// let p = Symbol("ahmed1223")
+// console.log(p, typeof (p));
+
+
+// let num1 = parseInt(prompt("Enter The Value :- "))
+// let num2 = parseInt(prompt("Enter The 2nd Value :- "))
+//console.log(`Sum OF Num 1 & Num 2 Is : ${num1 + num2}`);
+
+let name = "Ahmed Raza";
+let age = 21;
+let student = true
+console.log(name);
+console.log(age);
+console.log(student);
+
+var name1 = "Raza";
+console.log(name1);
+
+
+
+
+
+
