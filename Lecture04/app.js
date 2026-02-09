c86aec44302802db0fe792e0cee89fa411256d1a
ahmed04raxa <ahmed04raxa@gmail.com>
2026-02-09 09:05:17 +0500

    Lecture 04

---
+++ Lecture04/app.js
@@ -0,0 +1,52 @@
+// let users = ["Ahmed", "Raza", "Adnan"]
+
+// console.log(users[0]);
+// console.log(users[1]);
+// console.log(users[2]);
+
+// let colors = ["Red", "Green", "Blue"];
+// console.log(colors[0]);
+// console.log(colors[1]);
+// console.log(colors[2]);
+
+// let num = [10, 20, 30, [40, 50, 60]]
+// console.log(num[0]);
+// console.log(num[1]);
+// console.log(num[2]);
+// console.log(num[3][0]);
+// console.log(num[3][1]);
+// console.log(num[3][2]);
+//let num = [10, 20, 30]
+// let t = num.length
+// for (let i = t - 1; i >= 0; i--) {
+//     console.log(num[i]);
+// }
+// for (const v of num) {
+//     console.log(v);
+// }
+
+
+
+// let l = [10,20,30,40]
+// l.push(50)
+// l.push(60)
+// l.push(70,80,90)
+// console.log(l);
+
+// let arr = [1,2,3,4]
+// arr.shift()
+// // arr.pop()
+// // console.log(arr.pop());
+// console.log(arr);
+// arr.unshift(65)
+// console.log(arr);
+
+//let arr = [1,55,6,8,9,98,45,2,3,4]
+
+// arr.sort()
+// console.log(arr);
+
+
+// let l = ["Red","green","blue","yllow","purple"]
+// l.slice(1)
+// console.log(l);
