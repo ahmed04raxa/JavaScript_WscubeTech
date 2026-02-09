92371cd736c98fff782f259ddd91b7b5c67812ab
ahmed04raxa <ahmed04raxa@gmail.com>
2026-02-09 08:06:03 +0500

    Lecture 03

---
+++ Lecture03/app.js
@@ -0,0 +1,26 @@
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i, "Welcome");
+// }
+// console.log(i);
+
+// for (let s = 10; s >= 1; s--) {
+//     console.log(s, "Welcome");
+// }
+// console.log(s);
+
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i, "Even Number");
+//     } else {
+//         console.log(i, "Odd Number");
+//     }
+
+// }
+
+// let i = 1;
+
+// while (i <= 5) {
+//     console.log(i, "Welcome");
+//     i++;
+// }
