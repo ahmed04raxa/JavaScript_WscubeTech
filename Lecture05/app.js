42b33aad9de40b3945908c3f6ca585dc7daeb033
ahmed04raxa <ahmed04raxa@gmail.com>
2026-02-09 09:27:07 +0500

    Lecture 05

---
+++ Lecture05/app.js
@@ -0,0 +1,20 @@
+// function displayData() {
+//     document.writeln("Welcome To Ws")
+// }
+// displayData()
+// document.writeln("<br>")
+// displayData()
+// document.writeln("<br>")
+// function addData(a,b){
+//     document.writeln(a + b);
+//     document.writeln("<br>")
+// }
+// addData(4,5)
+// addData(6,5)
+// addData(478,5)
+
+// function sum(...allParam){
+//     console.log(allParam);
+    
+// }
+// sum(56,67,88,90,554,)
