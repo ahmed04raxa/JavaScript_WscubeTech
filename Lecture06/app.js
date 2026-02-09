84019e8551c72fb2feb84b921f3d9a28142fd521
ahmed04raxa <ahmed04raxa@gmail.com>
2026-02-09 09:57:55 +0500

    Lecture 06

---
+++ Lecture06/app.js
@@ -0,0 +1,35 @@
+let user = {
+    name: "Ahmed",
+    email: "ahmed@gmail.com",
+    phone: 9900990090,
+    age: 21,
+    userAddress: {
+        city: "Karachi",
+        province: "Sindh"
+    }
+}
+
+//console.log(user.name);
+
+// user.name = "raza"
+// console.log(user.name);
+
+
+// Destructure
+// let { name, email, phone, } = user;
+// console.log(name);
+
+// NESTED Destructure
+
+// let { userAddress: {
+//     city,
+//     province
+// } } = user
+// console.log(city);
+// console.log(province);
+
+
+// console.log(user.name);
+// console.log(user.age);
+// console.log(user.phone);
+// console.log(user.age);
