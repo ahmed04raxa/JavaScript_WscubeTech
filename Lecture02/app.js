32e0cb0ebf7d84e9df8cbef5eb1a537a48fd5c69
ahmed04raxa <ahmed04raxa@gmail.com>
2026-02-09 07:37:46 +0500

    Lecture 02

---
+++ Lecture02/app.js
@@ -0,0 +1,86 @@
+/*
+
+1) ARITHMETIC OPERATORS
+
+let a = 2;
+let b = 3;
+console.log(`Sum Of a & b Is :- ${a + b}`);
+console.log(`Substraction Of a & b Is :- ${a - b}`);
+console.log(`Multiplication Of a & b Is :- ${a * b}`);
+console.log(`Division Of a & b Is :- ${a / b}`);
+console.log(`Power Value Of a & b Is :- ${a ** b}`);
+console.log(`Modulo Value Is :- ${15 % 16}`);
+
+*/
+
+/*
+
+2) ASSIGNMNET OPERATORS
+
+
+*/
+
+// let n = 10;
+// console.log(n);
+// n = n + 5;
+// console.log(n);
+// n += 5
+// console.log(n);
+// n -= 5
+// console.log(n);
+// n /= 5
+// console.log(n);
+// n *= 5
+// console.log(n);
+// n %= 5
+// console.log(n);
+
+// let a = 10;
+// let b = 20
+// let temp = a;
+// a=b;
+// b=temp;
+// console.log("a",a);
+// console.log("b",b);
+// a= a+b;
+// b = a-b;
+// a = a-b
+// console.log(a);
+// console.log(b);
+
+// let score = 50
+
+// if (score >= 50) {
+//     console.log("You Passed");
+// }
+
+// let score = 60
+
+// if (score == 50) {
+//     console.log("You Passed");
+// } else {
+//     console.log("Fail");
+// }
+
+// let n = parseInt(prompt("Enter A Value :-"));
+// if (n % 2 == 0) {
+//     console.log("Even");
+// } else {
+//     console.log("Odd");
+
+// }
+
+let marks = 89;
+
+if (marks >= 90) {
+    console.log("A Grade");
+
+} else if (marks >= 70) {
+    console.log("B Grade");
+}
+else if (marks >= 50) {
+    console.log("C Grade");
+} else {
+    console.log("Fail");
+
+}
